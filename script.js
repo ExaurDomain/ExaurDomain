46d534f8773ff26b1053d2cfb8ea5dac85a0b2f8
ExaurDomain <148317134+ExaurDomain@users.noreply.github.com>
2023-10-19 15:31:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
+const loginModal = document.getElementById("login-modal");
+const loginButton = document.querySelector(".nav-links li a[href='#login']");
+const closeModalButton = document.getElementById("close-modal");
+
+loginButton.addEventListener("click", () => {
+    loginModal.style.display = "flex";
+});
+
+closeModalButton.addEventListener("click", () => {
+    loginModal.style.display = "none";
+});
+
+/*ABOUT MUSIC ARTIST */
+
 // JavaScript (script.js)
 document.addEventListener('DOMContentLoaded', function () {
-    // Show the artist container by default
-    var artistContainer = document.getElementById('artist-container');
-    artistContainer.style.display = 'flex';
-    
-    // Rest of your JavaScript code...
-    
-    // Function to toggle the visibility of the artist container
-    function toggleArtistContainer() {
-        if (artistContainer.style.display === 'none' || artistContainer.style.display === '') {
-            artistContainer.style.display = 'flex';
-        } else {
-            artistContainer.style.display = 'none';
-        }
-    }
+    const artistLinks = document.querySelectorAll('.artist-link');
 
-    // Add an event listener to the "Choose Artist" button (if you decide to add it back later)
-    var chooseArtistButton = document.getElementById('choose-artist-button');
-    chooseArtistButton.addEventListener('click', toggleArtistContainer);
+    artistLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const artistId = this.getAttribute('data-artist');
+            loadArtistPage(artistId);
+        });
+    });
+
+    function loadArtistPage(artistId) {
+        // Hide other sections (e.g., welcome and artist list)
+        document.getElementById('welcome').style.display = 'none';
+        document.getElementById('artist-list').style.display = 'none';
+
+        // Load the artist's page using AJAX or other methods
+        // For simplicity, let's assume the artist's pages are named artist1.html, artist2.html, etc.
+        const artistPageUrl = `${artistId}.html`;
+
+        // Fetch the artist's page content
+        fetch(artistPageUrl)
+            .then((response) => response.text())
+            .then((html) => {
+                // Display the artist's page content
+                const artistPageContainer = document.getElementById('artist-page-container');
+                artistPageContainer.innerHTML = html;
+                artistPageContainer.style.display = 'block';
+            })
+            .catch((error) => {
+                console.error(`Error loading artist page: ${error}`);
+            });
+    }
 });
+// Function to toggle the visibility of the artist container
+function toggleArtistContainer() {
+    var artistContainer = document.getElementById('artist-container');
+    if (artistContainer.style.display === 'none') {
+        artistContainer.style.display = 'flex';
+    } else {
+        artistContainer.style.display = 'none';
+    }
+}
+
+// Add an event listener to the "Choose Artist" button
+var chooseArtistButton = document.getElementById('choose-artist-button');
+chooseArtistButton.addEventListener('click', toggleArtistContainer);
